74afc84d7abf2f48e6051636086faa361b3b03b3
x8g <98675255+x8g@users.noreply.github.com>
2022-06-13 16:04:34 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import coochie
+from colorama import Fore
+
+def sayhello():
+    print(Fore.MAGENTA + "coochie py package by zt/x8g")
+    print(Fore.CYAN + "test upload to pypi, I will make beneficial packages soon!")
